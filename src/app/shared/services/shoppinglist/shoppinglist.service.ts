75a21246f4c644ae9e9e48b23891371053dc5b66
clara.rufi <clara.rufirull@clarivate.com>
2021-10-12 09:00:02 +0200

    modify some snack bar

--- src/app/shared/services/shoppinglist/shoppinglist.service.ts
+++ src/app/shared/services/shoppinglist/shoppinglist.service.ts
@@ -15,9 +15,9 @@ export class ShoppingListService {
     userCollection: AngularFirestoreCollection<NewShoppingItem>;
 
     constructor(
-        public afs: AngularFirestore,
-        public afAuth: AngularFireAuth,
-        public router: Router
+      public afs: AngularFirestore,
+      public afAuth: AngularFireAuth,
+      public router: Router
     ) {
 
         this.afAuth.authState.subscribe(user => {
@@ -31,52 +31,51 @@ export class ShoppingListService {
 
     public getShoppingUser(id: string): Observable<ShoppingList[]> {
         return this.afs.collection(id)
-            .snapshotChanges()
-            .pipe(
-                map(actions => actions.map( a => {
-                    const data = a.payload.doc.data() as ShoppingList;
-                    const idShoppingList = a.payload.doc.id;
-                    return {idShoppingList, ...data};
-                }))
-            );
-
+          .snapshotChanges()
+          .pipe(
+            map(actions => actions.map( a => {
+                const data = a.payload.doc.data() as ShoppingList;
+                const idShoppingList = a.payload.doc.id;
+                return {idShoppingList, ...data};
+            }))
+          );
     }
 
     public deleteItem(id): Promise<any> {
         return new Promise((resolve, reject) => {
             this.userCollection.doc(id).ref.get()
-                .then((doc) => {
-                if (doc.exists) {
-                    this.userCollection.doc(id).delete().then(() => {
-                        resolve('Item successfully deleted.');
-                        return this.userCollection.get();
-                    });
-                } else {
-                    reject('There has been an error. Try again. ');
-                }
-            });
+              .then((doc) => {
+                  if (doc.exists) {
+                      this.userCollection.doc(id).delete().then(() => {
+                         // resolve('Item successfully deleted.');
+                          return this.userCollection.get();
+                      });
+                  } else {
+                      reject('There has been an error. Try again. ');
+                  }
+              });
         });
     }
 
     public editItem(id, item): Promise<string> {
         return new Promise((resolve, reject) => {
             return this.userCollection.doc(id).update(item).then(() => {
-                resolve('This document has been correctly updated.');
+               // resolve('This document has been correctly updated.');
             })
-                .catch(() => {
-                    reject('There has been an error. Try again ');
-                });
+              .catch(() => {
+                  reject('There has been an error. Try again ');
+              });
         });
     }
 
     public newItem(item: NewShoppingItem): Promise<string> {
         return new Promise((resolve, reject) => {
-        this.userCollection.add(item).then(() => {
-            resolve('Item correctly created.');
-        })
-            .catch(() => {
-                reject('There has been an error. Try again ');
-            });
+            this.userCollection.add(item).then(() => {
+                // resolve('Item correctly created.');
+            })
+              .catch(() => {
+                  reject('There has been an error. Try again ');
+              });
         });
     }
 }
